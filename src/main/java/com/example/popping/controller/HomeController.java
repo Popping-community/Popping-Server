601694d7628c3fbf6fe94ebb0a1edba09bb303a8
chooh1010 <chooh1010@naver.com>
2025-05-05 08:51:25 +0900

    feat: login

--- src/main/java/com/example/popping/controller/HomeController.java
+++ src/main/java/com/example/popping/controller/HomeController.java
@@ -1,14 +1,26 @@
 package com.example.popping.controller;
 
 
+import com.example.popping.constant.SessionConst;
+import com.example.popping.domain.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.ui.Model;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
-        return "index";
+    public String home(
+            @SessionAttribute(name = SessionConst.LOGIN_USER, required = false)
+            User loginUser,
+            Model model) {
+        if (loginUser == null) {
+            return "home";
+        }
+
+        model.addAttribute("user", loginUser);
+        return "loginHome";
     }
 }
