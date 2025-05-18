e3dbc6eaccdc7f67e962988ec27e807363859a75
chooh1010 <chooh1010@naver.com>
2025-05-18 12:39:06 +0900

    refactor: login code

--- src/main/java/com/example/popping/domain/User.java
+++ src/main/java/com/example/popping/domain/User.java
@@ -1,16 +1,17 @@
 package com.example.popping.domain;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.io.Serial;
-import java.io.Serializable;
 
 @Entity
 @Builder
