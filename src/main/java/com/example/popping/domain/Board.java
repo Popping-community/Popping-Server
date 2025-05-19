308d51cfb19c3cf452871229810f2a303bf2e54e
chooh1010 <chooh1010@naver.com>
2025-05-19 10:32:45 +0900

    feat: post, board entity

---
+++ src/main/java/com/example/popping/domain/Board.java
@@ -0,0 +1,40 @@
+package com.example.popping.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.*;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Board extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    @Column(unique = true)
+    private String slug;
+
+    @OneToMany(mappedBy = "board")
+    private List<Post> posts = new ArrayList<>();
+
+    @ManyToOne(optional = false)
+    private User createdBy;
+
+    public void update(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+}
