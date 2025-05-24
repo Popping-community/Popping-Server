7d9ea6580c4557109a9eb1f324a4a8ebc4843269
chooh1010 <chooh1010@naver.com>
2025-05-25 01:15:20 +0900

    fix: board description, post content length limit

--- src/main/java/com/example/popping/dto/GuestPostCreateRequest.java
+++ src/main/java/com/example/popping/dto/GuestPostCreateRequest.java
@@ -1,5 +1,6 @@
 package com.example.popping.dto;
 
+import jakarta.persistence.Lob;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,6 +18,7 @@ public class GuestPostCreateRequest {
     @NotBlank(message = "제목은 필수입니다.")
     private String title;
 
+    @Lob
     @NotBlank(message = "내용은 필수입니다.")
     private String content;
 
