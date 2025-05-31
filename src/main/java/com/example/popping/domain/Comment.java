f1b4a5261b38e4efb606e49072e0ca05728726af
chooh1010 <chooh1010@naver.com>
2025-05-31 23:19:48 +0900

    feat: comment entity

---
+++ src/main/java/com/example/popping/domain/Comment.java
@@ -0,0 +1,55 @@
+package com.example.popping.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User author;
+
+    private String guestNickname;
+
+    private String guestPasswordHash;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Comment parent;
+
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> children = new ArrayList<>();
+
+    public boolean isAuthor(User user) {
+        return author != null && user != null && author.getId().equals(user.getId());
+    }
+
+    public boolean isGuest() {
+        return author == null;
+    }
+
+    public boolean isReply() {
+        return parent != null;
+    }
+}
