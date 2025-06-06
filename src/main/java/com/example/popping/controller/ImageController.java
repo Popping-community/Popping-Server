8daccfb18717e235892f466879ba1f892a003dc7
chooh1010 <chooh1010@naver.com>
2025-06-06 17:04:10 +0900

    feat: image controller

---
+++ src/main/java/com/example/popping/controller/ImageController.java
@@ -0,0 +1,27 @@
+package com.example.popping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import lombok.RequiredArgsConstructor;
+
+import com.example.popping.dto.ImageResponse;
+import com.example.popping.service.ImageService;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/images")
+public class ImageController {
+
+    private final ImageService imageService;
+
+    @PostMapping("/upload")
+    @ResponseBody
+    public ImageResponse uploadImage(@RequestParam("image") MultipartFile file) {
+        return imageService.uploadAndCreateTempImage(file);
+    }
+}
