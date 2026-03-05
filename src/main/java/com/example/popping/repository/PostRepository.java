2f5e5a245f457bca5c6d789030010dfe416f6455
chooh1010 <chooh1010@naver.com>
2026-03-06 03:01:32 +0900

    feat: author, board eager loading

--- src/main/java/com/example/popping/repository/PostRepository.java
+++ src/main/java/com/example/popping/repository/PostRepository.java
@@ -1,9 +1,10 @@
 package com.example.popping.repository;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -13,8 +14,11 @@ import com.example.popping.domain.Board;
 import com.example.popping.domain.Post;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-    List<Post> findAllByBoard(Board board);
 
+    @EntityGraph(attributePaths = {"author", "board"})
+    Optional<Post> findById(Long id);
+
+    @EntityGraph(attributePaths = {"author", "board"})
     Page<Post> findAllByBoard(Board board, Pageable pageable);
 
     @Modifying
