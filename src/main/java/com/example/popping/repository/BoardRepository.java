d8f4d49121470abde0edffe4df297405635a3476
chooh1010 <chooh1010@naver.com>
2026-03-07 21:50:09 +0900

    test: return to solving n+1 for jmeter test

--- src/main/java/com/example/popping/repository/BoardRepository.java
+++ src/main/java/com/example/popping/repository/BoardRepository.java
@@ -4,13 +4,16 @@ import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.popping.domain.Board;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
+    @EntityGraph(attributePaths = "createdBy")
     Optional<Board> findBySlug(String slug);
 
+    @EntityGraph(attributePaths = "createdBy")
     Page<Board> findAll(Pageable pageable);
 }
--- src/main/java/com/example/popping/repository/PostRepository.java
+++ src/main/java/com/example/popping/repository/PostRepository.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -14,8 +15,10 @@ import com.example.popping.domain.Post;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+    @EntityGraph(attributePaths = {"author", "board"})
     Optional<Post> findById(Long id);
 
+    @EntityGraph(attributePaths = {"author"})
     Page<Post> findAllByBoard(Board board, Pageable pageable);
 
     @Modifying
