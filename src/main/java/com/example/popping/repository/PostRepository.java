9dbf53de4d0babce62210c62c116f98fdae7e124
chooh1010 <chooh1010@naver.com>
2026-03-07 12:38:06 +0900

    test: return to n+1 for jmeter test

--- src/main/java/com/example/popping/repository/PostRepository.java
+++ src/main/java/com/example/popping/repository/PostRepository.java
@@ -4,7 +4,6 @@ import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -15,10 +14,8 @@ import com.example.popping.domain.Post;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    @EntityGraph(attributePaths = {"author", "board"})
     Optional<Post> findById(Long id);
 
-    @EntityGraph(attributePaths = {"author", "board"})
     Page<Post> findAllByBoard(Board board, Pageable pageable);
 
     @Modifying
