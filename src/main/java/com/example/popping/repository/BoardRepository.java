973ca3c52db8ba87d411311d311986a58fc09f53
chooh1010 <chooh1010@naver.com>
2026-03-21 09:12:33 +0900

    test: rollback for performance test

--- src/main/java/com/example/popping/repository/BoardRepository.java
+++ src/main/java/com/example/popping/repository/BoardRepository.java
@@ -4,20 +4,18 @@ import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.example.popping.domain.Board;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
-    @EntityGraph(attributePaths = "createdBy")
+    //@EntityGraph(attributePaths = "createdBy")
     Optional<Board> findBySlug(String slug);
 
-    @Query(
+    /*@Query(
         value = "SELECT b FROM Board b JOIN FETCH b.createdBy",
         countQuery = "SELECT COUNT(b) FROM Board b"
-    )
+    )*/
     Page<Board> findAll(Pageable pageable);
 }
