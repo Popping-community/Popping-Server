23d203fe424e66c7c609677dbbeb6d6de048323f
chooh1010 <chooh1010@naver.com>
2026-03-21 09:58:24 +0900

    test: rollback

--- src/main/java/com/example/popping/repository/PostRepository.java
+++ src/main/java/com/example/popping/repository/PostRepository.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -14,13 +15,13 @@ import com.example.popping.domain.Post;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    //@EntityGraph(attributePaths = {"author", "board"})
+    @EntityGraph(attributePaths = {"author", "board"})
     Optional<Post> findById(Long id);
 
-    /*@Query(
+    @Query(
         value = "SELECT p FROM Post p LEFT JOIN FETCH p.author WHERE p.board = :board",
         countQuery = "SELECT COUNT(p) FROM Post p WHERE p.board = :board"
-    )*/
+    )
     Page<Post> findAllByBoard(@Param("board") Board board, Pageable pageable);
 
     @Modifying(clearAutomatically = true, flushAutomatically = true)
