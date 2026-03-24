f9d9164ea31dc742a693fb93a61eb1f84a6e9596
chooh1010 <chooh1010@naver.com>
2026-03-24 09:12:09 +0900

    feat: add PostListItemResponse

--- src/main/java/com/example/popping/dto/PostPageResponse.java
+++ src/main/java/com/example/popping/dto/PostPageResponse.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public record PostPageResponse(
 
-        List<PostResponse> posts,
+        List<PostListItemResponse> posts,
         int totalPosts,
         int currentPage,
         int totalPages,
